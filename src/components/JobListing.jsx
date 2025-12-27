61beb6dfca94e119c59f2f3186bcc0892a7ef7bd
Marcello C <marcello_ct@hotmail.com>
2025-12-27 17:27:33 -0300

    Adding useState to show description

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
+
 const JobListing = ({ job }) => {
+  // [name of the state, name of the function thta we call to change that state]
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  // if false
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -7,7 +19,14 @@ const JobListing = ({ job }) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
+
+        <button
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? "Less" : "More"}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -16,6 +35,8 @@ const JobListing = ({ job }) => {
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
             <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />{" "}
+            {/* react icons built in component */}
             {job.location}
           </div>
           <a
