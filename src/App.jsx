9c2b91e972b35712eecabc4ceb08e3ed67afb244
Marcello C <marcello_ct@hotmail.com>
2025-12-27 00:59:49 -0300

    Creating Components for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+
 function App() {
   return (
     <>
-      <div>App</div>
+      <Navbar />
+      <Hero
+        title="Become a React Dev"
+        subtitle="Find the React job that fits your skills and needs"
+      />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   );
 }
