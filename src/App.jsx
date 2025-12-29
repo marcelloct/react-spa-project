49ba7c38b1ebf2f380538c69749c1eda5ec74b69
Marcello C <marcello_ct@hotmail.com>
2025-12-29 18:05:37 -0300

    DELETE Request to Remove Job

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJobPage from "./pages/AddJobPage";
 
 function App() {
+  // Add new job
   const addJob = async (newJob) => {
     // console.log(newJob);
     const res = await fetch("/api/jobs", {
@@ -24,12 +25,25 @@ function App() {
     return;
   };
 
+  // Delete Job
+  const deleteJob = async (id) => {
+    // console.log("delete", id);
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
-        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path="*" element={<NotFound />} />
       </Route>
